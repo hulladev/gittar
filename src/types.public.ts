24a839be09fa471f5f15cab5ed2f076d22414690
samuelhulla <hulla@hulla.dev>
2026-01-02 21:59:16 +0100

    :wrench: update: fmt

--- src/types.public.ts
+++ src/types.public.ts
@@ -8,9 +8,9 @@ export type Config = {
 }
 
 export type GittarResult = {
-  files: string[]       // Absolute file paths (filtered by subpath if specified)
-  cacheDir: string      // Where the full tar is stored
-  outDir: string        // Where files were copied to (may be same as cacheDir)
-  subpath?: string      // The subpath filter if user specified one
-  fromCache: boolean    // Whether data was retrieved from cache (true) or downloaded (false)
+  files: string[] // Absolute file paths (filtered by subpath if specified)
+  cacheDir: string // Where the full tar is stored
+  outDir: string // Where files were copied to (may be same as cacheDir)
+  subpath?: string // The subpath filter if user specified one
+  fromCache: boolean // Whether data was retrieved from cache (true) or downloaded (false)
 }
